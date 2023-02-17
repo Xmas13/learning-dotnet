f7c619c567f49a0b848c1685b7178f4a00dcdbaa
Noel Miller <noelmiller@protonmail.com>
2023-02-17 11:11:28 -0600

    Initial

---
+++ SimpleMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld() 
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
