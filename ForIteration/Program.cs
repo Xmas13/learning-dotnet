a26ad52a58ea1d00d4f0c4fe64cdab7853c19109
Noel Miller <noelmiller@protonmail.com>
2023-02-16 22:51:58 -0600

    initial

---
+++ ForIteration/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForIteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+            Console.ReadLine();
+        }
+    }
+}
