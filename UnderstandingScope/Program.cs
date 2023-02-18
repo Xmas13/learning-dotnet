e4e7adb7b64234f62ee4f5fae9f471c0f446fc34
Noel Miller <noelmiller@protonmail.com>
2023-02-18 14:43:17 -0600

    Learned about scope!

---
+++ UnderstandingScope/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace UnderstandingScope
+{
+    internal class Program
+    {
+        private static string k = "";
+        
+        static void Main(string[] args)
+        {
+            string j = "";
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+                // Console.WriteLine(l); // Does not work because it exists outside the context of an if statement
+            }
+            // Console.WriteLine(i); // Will not be abled to access i since it is out of the scope of the code block
+            Console.WriteLine("Outside of the for: " + j);
+            Console.WriteLine("Outside of the for: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            
+            myCar.DoSomething();
+
+            // myCar.helperMethod(); // This would not work because the helperMethod is not public
+
+            Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("Value of k from the HelperMethod(): " + k);
+        }
+    }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod()
+        {
+            return "Hello World";
+        }
+    }
+}
