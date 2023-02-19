0554dd42659c289a2dfd7a45819f56112bbd14e0
Noel Miller <noelmiller@protonmail.com>
2023-02-19 16:34:21 -0600

    Learned about handling exceptions

---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace HandlingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Users\noelm\Exampl.txt");
+                Console.WriteLine(content);
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is correct!");
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the Directory is correct!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                // Code to finalize
+                // Setting objects to null
+                // Closing database connections
+                Console.WriteLine("Closing application now...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
