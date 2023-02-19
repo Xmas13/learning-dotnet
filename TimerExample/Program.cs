39fe64319a7677b763e4f25167308be5c8bbb1b5
Noel Miller <noelmiller@protonmail.com>
2023-02-19 17:05:47 -0600

    Learned how to handle events! And I finished the course!

---
+++ TimerExample/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TimerExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(2000);
+
+            timer.Elapsed += Timer_Elapsed;
+            timer.Elapsed += Timer_Elapsed1;
+
+            timer.Start();
+
+            Console.WriteLine("Press enter to remove the red event.");
+            Console.ReadLine();
+            timer.Elapsed -= Timer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void Timer_Elapsed1(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void Timer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+    }
+}
